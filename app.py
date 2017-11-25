feda70f9e307763e13f07702a78e331f73b14618
TheCPPanda <rico.lang15@gmail.com>
2017-11-25 17:50:36 +0100

    Move test code

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMainWindow, qApp, QAction, QPushButton
 from autobahn.twisted.wamp import ApplicationRunner, ApplicationSession
+from autobahn import wamp
+from twisted.internet.defer import inlineCallbacks, returnValue
 
 import qt5reactor
 
@@ -14,13 +16,20 @@ class MainWindow(QMainWindow, ApplicationSession):
 
         test_button = QPushButton("Execute RPC", self)
         test_button.move(50, 50)
+        test_button.clicked.connect(self.on_test_button_clicked)
 
         self.setGeometry(300, 300, 300, 200)
         self.setWindowTitle("Simple Menu")
         self.show()
 
+    @inlineCallbacks
+    def whatever_test(self):
+        response = yield self.call(u"wamp.registration.list")
+        returnValue(response)
+
     def on_test_button_clicked(self):
-        pass
+        d = self.whatever_test()
+        d.addCallback(lambda x: print(x))
 
     def closeEvent(self, event):
         self.leave()
@@ -31,6 +40,10 @@ class MainWindow(QMainWindow, ApplicationSession):
         if reactor.threadpool is not None:
             reactor.threadpool.stop()
         qApp.quit()
+
+    @wamp.register(u"com.rlang.test_function")
+    def test_function(data):
+        print(data)
 
 
 def make(config):
@@ -44,7 +57,8 @@ def main():
     app = QtWidgets.QApplication(sys.argv)
     qt5reactor.install()
 
-    runner = ApplicationRunner(url=u"ws://127.0.0.1:8080", realm=u"realm1")
+    runner = ApplicationRunner(url=u"ws://192.168.178.52:8080",
+                               realm=u"realm1")
     runner.run(make)
 
 
